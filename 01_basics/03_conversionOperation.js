051b29dcae430ac1d2a0538ce2d86696c79e1f67
Deepak Mishra <55850598+crazydevjs@users.noreply.github.com>
2023-10-30 20:11:47 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = undefined
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 // false => 0
+
+let isLoggedIn = 1
+
+let booleanisLogged = Boolean(isLoggedIn)
+console.log(booleanisLogged);
+
+// 1 => true // 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+console.log(stringNumber);
