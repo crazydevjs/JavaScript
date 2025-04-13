1f8b0bf2acb29ecd64378d7d8a269b8e2e91f7d7
Deepak Mishra <dt1702281@gmail.com>
2025-04-13 13:04:40 +0530

    whiledo

---
+++ 05_iterations/two.js
@@ -0,0 +1,20 @@
+let index = 0;
+while (index <= 10) {
+    console.log(`Value of index: ${index}`);
+    index = index + 2;
+    
+}
+
+let myArray = ["flash", "batman", "superman"];
+let arr = 0;
+while(arr < myArray.length) {
+    console.log(`Superhero: ${myArray[arr]}`);
+    arr++;
+    
+}
+let score = 11;
+do {
+    console.log(`Score: ${score}`);
+    score++;
+    
+} while (score <= 10);
